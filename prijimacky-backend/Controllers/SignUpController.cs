03c964af954c3d37de5a13e3940ca20d117fbc59
aeggydev <kucernak.a@gmail.com>
2022-03-20 15:39:39 +0100

    Add a database and a signup controller

---
+++ prijimacky-backend/Controllers/SignUpController.cs
@@ -0,0 +1,33 @@
+﻿using System.Text.Json;
+using Microsoft.AspNetCore.Mvc;
+using prijimacky_backend.Data;
+
+namespace prijimacky_backend.Controllers;
+
+public record NewParticipant(
+    string ParticipantName,
+    string ParticipantSurname,
+    string ParentName,
+    string ParentSurname,
+    string School,
+    string Phone,
+    string Email);
+
+[ApiController]
+[Route("[controller]")]
+public class SignUpController
+{
+    private readonly ApplicationDbContext _dbContext;
+
+    public SignUpController(ApplicationDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
+    [HttpPost]
+    public NewParticipant SignUp(NewParticipant newParticipant)
+    {
+        Console.WriteLine(JsonSerializer.Serialize(newParticipant));
+        return newParticipant;
+    }
+}
---
+++ prijimacky-backend/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using prijimacky_backend.Entities;
+
+namespace prijimacky_backend.Data;
+
+public class ApplicationDbContext : DbContext
+{
+    public DbSet<Participant> Participants { get; set; } = default!;
+
+    protected override void OnConfiguring(DbContextOptionsBuilder options)
+    {
+        options.UseSqlite("Date Source=Application.db");
+    }
+}
