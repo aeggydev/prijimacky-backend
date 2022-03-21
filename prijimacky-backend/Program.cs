088143cde0964cda032354f68508e7f57cc79b92
aeggydev <kucernak.a@gmail.com>
2022-03-21 17:37:33 +0100

    Add graphql and disable swagger

--- prijimacky-backend/Program.cs
+++ prijimacky-backend/Program.cs
@@ -1,28 +1,23 @@
 using prijimacky_backend.Data;
+using prijimacky_backend.Graphql;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddGraphQLServer()
+    .AddQueryType<Query>();
 builder.Services.AddDbContext<ApplicationDbContext>();
 
 var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
 
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
 app.MapControllers();
+
+app.MapGraphQL();
 
 app.Run();
