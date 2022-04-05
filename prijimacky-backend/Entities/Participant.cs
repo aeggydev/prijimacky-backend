6f3fb3da2de908964d1aacf9f30b9a2f15c6488a
aeggydev <kucernak.a@gmail.com>
2022-04-05 12:22:17 +0200

    Add header names to all fields in Participant.cs

--- prijimacky-backend/Entities/Participant.cs
+++ prijimacky-backend/Entities/Participant.cs
@@ -1,19 +1,21 @@
-﻿namespace prijimacky_backend.Entities;
+﻿using CsvHelper.Configuration.Attributes;
+
+namespace prijimacky_backend.Entities;
 
 public class Participant
 {
     // TODO: Add admin-facing ID
-    public int Id { get; set; }
-    public string ParticipantName { get; set; } = "";
-    public string ParticipantSurname { get; set; } = "";
-    public string ParentName { get; set; } = "";
-    public string ParentSurname { get; set; } = "";
-    public string School { get; set; } = "";
-    public string Phone { get; set; } = "";
-    public string Email { get; set; } = "";
-    public string Ip { get; set; } = "";
-    public string VariableSymbol { get; set; } = "";
-    public DateTime SignUpDate { get; set; }
-    public DateTime DueDate => SignUpDate.AddDays(15);
-    public DateTime? PaidDate { get; set; }
+    [Name("Id")] public int Id { get; set; }
+    [Name("Jméno účastníka")] public string ParticipantName { get; set; } = "";
+    [Name("Příjmení účastníka")] public string ParticipantSurname { get; set; } = "";
+    [Name("Jméno zákonného zástupce")] public string ParentName { get; set; } = "";
+    [Name("Příjmení zákonného zástupce")] public string ParentSurname { get; set; } = "";
+    [Name("Škola")] public string School { get; set; } = "";
+    [Name("Telefonní číslo")] public string Phone { get; set; } = "";
+    [Name("E-mailová adresa")] public string Email { get; set; } = "";
+    [Name("IP adresa")] public string Ip { get; set; } = "";
+    [Name("Variabilní symbol")] public string VariableSymbol { get; set; } = "";
+    [Name("Datum přihlášení")] public DateTime SignUpDate { get; set; }
+    [Name("Datum splatnosti")] public DateTime DueDate => SignUpDate.AddDays(15);
+    [Name("Datum uhrazení")] public DateTime? PaidDate { get; set; }
 }
